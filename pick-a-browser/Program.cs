881920a90c39af76123c7c053a916b6d058356eb
Stuart Leeks <stuart@leeks.net>
2022-01-10 18:21:14 +0000

    Match URL against rules

--- pick-a-browser/Program.cs
+++ pick-a-browser/Program.cs
@@ -37,7 +37,29 @@ namespace pick_a_browser
             var settings = await Settings.LoadAsync();
 
             var browsers = settings.Browsers.ToList();
-            var url = "https://wsl.tips/";
+            var url = args.Length > 0 ? args[0] : "";
+
+            if (url != "")
+            {
+                var uri = new Uri(url);
+
+                // Get matches with highest weights (handle multiple matches with the same weight)
+                var matchedBrowserNames = settings.Rules
+                        .Select(r => r.GetMatch(uri))
+                        .Where(m => m.Weight > 0)
+                        .GroupBy(m => m.Weight)
+                        .OrderByDescending(g => g.Key)
+                        // Take the top weighted match(es)
+                        ?.FirstOrDefault()
+                        // Get browsers
+                        ?.Select(m => m.BrowserName)
+                        ?.Distinct();
+
+                if (matchedBrowserNames != null)
+                {
+                    browsers = browsers.Where(b => matchedBrowserNames.Contains(b.Name)).ToList();
+                }
+            }
 
             RunWpfApp(() =>
             {
