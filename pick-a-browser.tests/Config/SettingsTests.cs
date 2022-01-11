79a18fdcef750cfe40500071e0b33039e4753862
Stuart Leeks <stuart@leeks.net>
2022-01-11 12:58:29 +0000

    Fix up tests

--- pick-a-browser.tests/Config/SettingsTests.cs
+++ pick-a-browser.tests/Config/SettingsTests.cs
@@ -11,14 +11,17 @@ namespace pick_a_browser.tests.Config
             var json = @"{
     ""browsers"": [
         {
+            ""id"" : ""test1"",
             ""name"" : ""test1"",
             ""exe"" : ""test1_exe"",
         },
         {
+            ""id"" : ""test2"",
             ""name"" : ""test2"",
             ""exe"" : ""test2_exe"",
             ""args"" : ""arg1 arg2"",
             ""iconPath"" : ""c:\\some\\path"",
+            ""hidden"" : true,
         }
     ]
 }";
@@ -30,17 +33,21 @@ namespace pick_a_browser.tests.Config
 
             var browser = settings.Browsers[0];
             Assert.NotNull(browser);
+            Assert.Equal("test1", browser.Id);
             Assert.Equal("test1", browser.Name);
             Assert.Equal("test1_exe", browser.Exe);
             Assert.Null(browser.Args);
             Assert.Null(browser.IconPath);
+            Assert.False(browser.Hidden);
 
             browser = settings.Browsers[1];
             Assert.NotNull(browser);
+            Assert.Equal("test2", browser.Id);
             Assert.Equal("test2", browser.Name);
             Assert.Equal("test2_exe", browser.Exe);
             Assert.Equal("arg1 arg2", browser.Args);
             Assert.Equal("c:\\some\\path", browser.IconPath);
+            Assert.True(browser.Hidden);
         }
 
         [Fact]
@@ -69,12 +76,12 @@ namespace pick_a_browser.tests.Config
 
             var prefixRule = settings.Rules[0] as PrefixRule;
             Assert.NotNull(prefixRule);
-            Assert.Equal("https://example.com", prefixRule.PrefixMatch);
+            Assert.Equal("https://example.com", prefixRule!.PrefixMatch);
             Assert.Equal("browser1", prefixRule.Browser);
 
             var hostRule = settings.Rules[1] as HostRule;
             Assert.NotNull(hostRule);
-            Assert.Equal("example.com", hostRule.Host);
+            Assert.Equal("example.com", hostRule!.Host);
             Assert.Equal("browser2", hostRule.Browser);
         }
     }
